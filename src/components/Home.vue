ab119930bbbe3d8549970a712ea6038dc04b1f0d
laitmatus <laitmatus@pop-os.localdomain>
2022-01-23 01:43:21 +0530

    first commit vuetify added.

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+        {{msg}}
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+
+    data: () => ({
+      msg: "Welcome To The Home Page."
+    }),
+  }
+</script>
